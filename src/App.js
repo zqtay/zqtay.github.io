bb9ad6cb036fb37d2fdccc8ffe19f619af9bb1c3
zqtay <zqtay96@gmail.com>
2022-08-05 00:47:53 +0800

    Change to single page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
 
-// Router
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 // Bootstrap
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,55 +7,73 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import Home from './component/Home';
-import HomeHeader from './component/HomeHeader';
 import Resume from './component/Resume';
 import Project from './component/Project';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <AppHeader />
-        <AppBody />
-      </BrowserRouter>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentBody: <Home />};
+    this.setCurrentBody = this.setCurrentBody.bind(this)
+  }
 
-function AppHeader() {
-  return (
-    <Routes>
-      <Route index element={
+  setCurrentBody(selectedKey) {
+    var body = <Home />;
+    switch (selectedKey) {
+      case "home":
+        body = <Home />;
+        break;
+      case "resume":
+        body = <Resume />;
+        break;
+      case "project1":
+      case "project2":
+      case "project3":
+        body = <Project />;
+        break;
+      default:
+        break;
+    }
+    this.setState(
+      {
+        currentBody: body
+      }
+    );
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }
+
+  render() {
+    return (
+      <div className="App">
         <header className='sticky-top'>
-          <AppNavBar variant='dark' bg='dark' />
+          <AppNavBar variant='dark' bg='dark' handleClick={this.setCurrentBody}/>
         </header>
-      } />
-      <Route path="*" element={    
-        <header className='sticky-top'>
-          <AppNavBar variant='light' bg='light'/>
-          <div className="mb-3" />
-        </header>
-      } />
-    </Routes>
-  );
+        <AppBody currentBody={this.state.currentBody}/>
+        <footer>
+          <AppFooter />
+        </footer>
+      </div>
+    );
+  }
 }
 
 function AppNavBar(props) {
   var variant = (props.variant === 'dark') ? 'dark' : 'light';
   var style = props.bg === 'transparent' ? {backgroundColor: "#00000000"} : {};
   return (
-    <Navbar variant={variant} bg={props.bg} expand="md" stlye={style}>
+    <Navbar collapseOnSelect variant={variant} bg={props.bg} expand="md" stlye={style}>
       <Container>
-        <Navbar.Brand href="/">My Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/resume">Resume</Nav.Link>
+        <Navbar.Brand href="#">My Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" onSelect={props.handleClick}>
+            <Nav.Link href="#" eventKey="home">Home</Nav.Link>
+            <Nav.Link href="#" eventKey="resume">Resume</Nav.Link>
             <NavDropdown title="Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item href="project/1">Project 1</NavDropdown.Item>
-              <NavDropdown.Item href="project/2">Project 2</NavDropdown.Item>
-              <NavDropdown.Item href="project/3">Project 3</NavDropdown.Item>
+              <NavDropdown.Item href="#" eventKey="project1">Project 1</NavDropdown.Item>
+              <NavDropdown.Item href="#" eventKey="project2">Project 2</NavDropdown.Item>
+              <NavDropdown.Item href="#" eventKey="project3">Project 3</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
@@ -67,21 +82,14 @@ function AppNavBar(props) {
   );
 }
 
-function AppBody() {
+function AppBody(props){
   return (
-    <Container fluid className="px-0">
-      <Routes>
-        <Route index element={<HomeHeader />} />
-      </Routes>
-      <Container>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path='resume' element={<Resume />} />
-            <Route path='project/*' element={<Project />} />
-          </Routes>
-      </Container>
-    </Container>
+    props.currentBody
   );
+}
+
+function AppFooter(props) {
+  return;
 }
 
 export default App;
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import HomeHeader from './HomeHeader';
 
 class Home extends React.Component {
     render() {
         return (
-            <Container>
-                <div class="shadow p-3 mb-5 bg-body rounded" 
-                style={{height: "100vh", backgroundColor:"#FFFFFFFF", transform:"translate(0px, -8%)"}}>      
-                    <div className='h2' align='left'>Home</div>
-                </div>
-            </Container>
+            <div>
+                <HomeHeader />
+                <Container>
+                    <div className="shadow p-3 mb-5 bg-body rounded" 
+                    style={{height: "100vh", backgroundColor:"#FFFFFFFF", transform:"translate(0px, -8%)"}}>      
+                        <div className='h2' align='left'>Home</div>
+                    </div>
+                </Container>
+            </div>
         );
     }
 }
